6ddd4a5ebe0199334fc179659256cdfb60f8c46e
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-06-25 10:37:09 +0200

    Fix them resizable crash and invalid HTML

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // JavaScript Document
 window.onload = function() {
-	var container = document.querySelector('ul.js-masonry');
+	var container = document.querySelector('.js-masonry');
 	var msnry = new Masonry( container, {
 	  columnWidth: ".sizer",
 	  gutter: ".gutter",
-	  itemSelector: "li",
+	  itemSelector: ".tile"
 	}).layout();
 }
